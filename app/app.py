58fdecf40f2c34b71932e35bf4411e7841ffd1fb
abhishekchandra2522k <1ac23456789@gmail.com>
2022-04-15 17:20:56 +0530

    Add Fertilizers Recommendation page and features

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, Markup, redirect
+from flask import Flask, render_template, request, Markup #, redirect
 import numpy as np
 import pandas as pd
 import requests
 import config
 import pickle
-import io   
+# import io   
+from utils.fertilizer import fertilizer_dic
 
 # Loading crop recommendation model
 
@@ -55,6 +56,14 @@ def crop_recommend():
     title = 'Crop Recommendation'
     return render_template('crop.html', title=title)
 
+# render fertilizer recommendation form page
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
+
 # ----------------------- RENDER PREDICTION PAGES ------------------------------------
 
 # render crop recommendation result page
@@ -85,6 +94,49 @@ def crop_prediction():
 
             return render_template('try_again.html', title=title)
 
+
+# render fertilizer recommendation result page
+
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'Fertilizer Suggestion'
+
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    # ph = float(request.form['ph'])
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer-result.html', recommendation=response, title=title)
 
 # Main method
 if __name__ == '__main__':
